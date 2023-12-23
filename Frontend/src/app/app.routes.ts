8fa8e1f23ef212e16e266592fbf6ed344a854b02
0KevinB <92813801+0KevinB@users.noreply.github.com>
2023-12-23 16:05:41 -0500

    Vistas

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -6,12 +6,13 @@ import { RegisterComponent } from './shared/register/register.component';
 
 
 export const routes: Routes = [
-  {path:'',redirectTo:'/inicio', pathMatch:'full'},
-  {path:'inicio',component:DashboardComponent},
-  {path:'login',component:LoginComponent},
-  {path:'forgot-password',component:NewpasswordComponent},
-  {path:'forgot-password',redirectTo:'/forgot-password', pathMatch:'full'},
-  {path:'register',component:RegisterComponent},
-  {path:'register',redirectTo:'/register', pathMatch:'full'}
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'forgot-password', component: NewpasswordComponent },
+  { path: 'forgot-password', redirectTo: '/forgot-password', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
+  { path: 'register', redirectTo: '/register', pathMatch: 'full' },
+  { path: '**', redirectTo: '/inicio', pathMatch: 'full' }
 ];
 
