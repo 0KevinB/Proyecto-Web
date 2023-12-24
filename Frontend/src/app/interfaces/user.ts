99d88c0e375354626e7ed767e64f9b735c4562a0
0KevinB <92813801+0KevinB@users.noreply.github.com>
2023-12-23 20:53:58 -0500

    Test

--- Frontend/src/app/interfaces/user.ts
+++ Frontend/src/app/interfaces/user.ts
@@ -1,13 +1,13 @@
 export interface User{
-    Cedula : string;
-    Nombre : string;
-    Apellido : string;
-    CorreoElectronico : string;
-    Contraseña: string;
-    Direccion : string;
-    Telefono : string;
-    Estado: Boolean;
-    RolID : Number;
-    createAt: Date;
-    updateAt: Date;
+    Cedula? : string;
+    Nombre? : string;
+    Apellido? : string;
+    CorreoElectronico? : string;
+    Contraseña?: string;
+    Direccion? : string;
+    Telefono? : string;
+    Estado?: Boolean;
+    RolID? : Number;
+    createAt?: Date;
+    updateAt?: Date;
 }
